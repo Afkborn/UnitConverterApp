8be7951cce245bbcd37316f16919684e53442bbf
Bilgehan K <kalaybilgehan60@gmail.com>
2021-10-12 23:25:57 +0300

    master

--- app/src/main/java/com/bilgehankalay/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/bilgehankalay/unitconverterapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.bilgehankalay.unitconverterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bilgehankalay.unitconverterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.metersButton.setOnClickListener {
+            val intent = Intent(this,MetersActivity::class.java)
+            startActivity(intent)
+        }
+        binding.temperatureButton.setOnClickListener {
+            val intent = Intent(this,TemperatureActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/bilgehankalay/unitconverterapp/MetersActivity.kt
@@ -0,0 +1,11 @@
+package com.bilgehankalay.unitconverterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MetersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meters)
+    }
+}
